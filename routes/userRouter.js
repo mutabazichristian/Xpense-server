628899a5ab803fb9ad12295c0dbc6f149e423722
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-29 10:51:42 +0200

    fix

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,23 +5,21 @@ const { login } = require('../controllers/authController');
 
 // Define a route for handling login requests
 router.post('/', async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const loginResult = await login(email, password);
-  
-      console.log('is it a success',loginResult.success)
-      if (loginResult.success) {
-        res.status(200).json({...loginResult.data});
-        console.log(loginResult.data)
-      } else {
-        res.status(401).json(loginResult.data);
-      }
-    } catch (error) {
-      console.error('Router error:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
+  try {
+    const { email, password } = req.body;
+    const loginResult = await login(email, password);
+
+    console.log('is it a success', loginResult.success)
+    if (loginResult.success) {
+      res.status(200).json({ ...loginResult.data });
+      console.log(loginResult.data)
     }
-  });
-  
+  } catch (error) {
+    console.error('Router error:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 
 module.exports = router;
 
