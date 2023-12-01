ca64bb3d1961e2c5eceb9c0b43cafb3a93921fe4
mutabazichristian <c.mutabazi@alustudent.com>
2023-12-01 10:13:20 +0200

    a

--- controllers/useradminController.js
+++ controllers/useradminController.js
@@ -1,7 +1,9 @@
 const { UserAdmin } = require('../models/index');
 
 const getUserAdmins = async (req, res) => {
-    res.json({message: 'This is the controller handling user admins'});
+    res.json({ message: 'This is the controller handling user admins' });
+    const variable = UserAdmin.findAll();
+    console.log(variable)
 }
 
 
